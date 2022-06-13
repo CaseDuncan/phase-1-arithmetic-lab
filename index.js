d2971e24949f7785744a36593d8682e50aebd18b
devdan <oluochdancun19@gmail.com>
2022-06-13 08:31:53 +0300

    commit tree 7fac077e48a455d1c362faea0d8d65a4b63420bd

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 =2
+const num2 = 31
+let multiply  =num1*num2;
+// console.log(multiply)
+
+const random = Math.floor(Math.random()*10)
+// console.log(random)
+
+const num3 = 11
+const num4 = 7
+
+const mod = num3 % num4
+// console.log(mod)
+
+const max = Math.max(1, 2, 5, 7, 10, 20)
+// console.log(max)
